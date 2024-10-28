b5e97a7f961d09c048e4b13554518cede3d934dd
Brian Schneider de Lima <bschneiderlima@gmail.com>
2024-10-28 16:09:55 -0700

    Thirty eighth commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,110 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    const navigateTo = url => {
-        history.pushState(null, null, url);
-        router();
-    };
-
-    const router = async () => {
-        const routes = [
-            { path: "/", view: () => "Welcome to the Home Page" },
-            { path: "/pages/about.html", view: () => "Learn more About Us" },
-            { path: "/pages/contact.html", view: () => "Contact Us Here" },
-            { path: "/pages/login.html", view: () => "Login to Your Account" }
-        ];
-
-        const potentialMatches = routes.map(route => {
-            return {
-                route: route,
-                isMatch: location.pathname === route.path
-            };
-        });
-
-        let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
-
-        if (!match) {
-            match = {
-                route: routes[0],
-                isMatch: true
-            };
-        }
-
-        document.querySelector("#app").innerHTML = await match.route.view();
-    };
-
-    window.addEventListener("popstate", router);
-
-    document.body.addEventListener("click", e => {
-        if (e.target.matches("[data-link]")) {
-            e.preventDefault();
-            navigateTo(e.target.href);
-        }
-    });
-
-    router();
-    
-    requireAuth();
-
-    const currentTimeElement = document.getElementById('current-time');
-    const historyList = document.getElementById('history-list');
-
     const reportForm = document.getElementById('report-form');
     const reportTableBody = document.querySelector('#report-table tbody');
-    
     const baseUrl = 'http://127.0.0.1:5000';
-
-    const saveTimeEndpoint = `${baseUrl}/save_time`;
-    const loadHistoryEndpoint = `${baseUrl}/object`;
-
-
-    let currentEntry = {
-        user: localStorage.getItem('id'),
-        date: new Date().toLocaleDateString(),
-        entrada: '',
-        intervalo: '',
-        retorno: '',
-        saida: ''
-    };
-
-    function updateTime() {
-        const now = new Date();
-        currentTimeElement.textContent = now.toLocaleString();
-    }
-
-    async function addHistoryEntry(timeField) {
-        const now = new Date();
-        const timeValue = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hourCycle: 'h23' });
-    
-        // Send the entry to the backend
-        try {
-            const response = await fetch(saveTimeEndpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    user: currentEntry.user,
-                    date: currentEntry.date,
-                    time_field: timeField,
-                    time_value: timeValue
-                })
-            });
-    
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-    
-            // Refresh the table to show the latest data
-            const userId = localStorage.getItem('id'); // Adjust this line based on where you store the userId
-            if (userId) {
-                await loadHistoryUser(userId);
-            } else {
-                console.error('User ID not found');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    }
 
     async function fetchReport(employee, period) {
         try {
@@ -142,23 +39,54 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    async function loadHistory() {
-        // Retrieve the history from the backend
+    const currentTimeElement = document.getElementById('current-time');
+    const historyList = document.getElementById('history-list');
+
+    const saveTimeEndpoint = `${baseUrl}/save_time`;
+    const loadHistoryEndpoint = `${baseUrl}/object`;
+
+    let currentEntry = {
+        user: localStorage.getItem('id'),
+        date: new Date().toLocaleDateString(),
+        entrada: '',
+        intervalo: '',
+        retorno: '',
+        saida: ''
+    };
+
+    function updateTime() {
+        const now = new Date();
+        currentTimeElement.textContent = now.toLocaleString();
+    }
+
+    async function addHistoryEntry(timeField) {
+        const now = new Date();
+        const timeValue = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hourCycle: 'h23' });
+
         try {
-            const response = await fetch(baseUrl);
+            const response = await fetch(saveTimeEndpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user: currentEntry.user,
+                    date: currentEntry.date,
+                    time_field: timeField,
+                    time_value: timeValue
+                })
+            });
+
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
 
-            const data = await response.json();
-            historyList.innerHTML = ''; // Clear the current list
-
-            // Populate the history list with the retrieved data
-            data.forEach(entry => {
-                const listItem = document.createElement('li');
-                listItem.textContent = `${entry.type} - ${new Date(entry.timestamp).toLocaleString()}`;
-                historyList.appendChild(listItem);
-            });
+            const userId = localStorage.getItem('id');
+            if (userId) {
+                await loadHistoryUser(userId);
+            } else {
+                console.error('User ID not found');
+            }
         } catch (error) {
             console.error('Error:', error);
         }
@@ -170,34 +98,26 @@ document.addEventListener('DOMContentLoaded', () => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-    
+
             const data = await response.json();
             const historyTableBody = document.querySelector('#history-table tbody');
             historyTableBody.innerHTML = ''; // Clear the current table body
-    
-            // Get the current date and the date 5 days prior
+
             const currentDate = new Date();
             const pastDate = new Date();
             pastDate.setDate(currentDate.getDate() - 6);
-    
-            // Filter the data to include only entries within the desired date range
+
             const filteredData = data.filter(entry => {
                 const entryDate = new Date(entry.date);
                 return entryDate >= pastDate && entryDate <= currentDate;
             });
-    
-            // Sort the filtered data in descending order by date
-            filteredData.sort((a, b) => new Date(b.date) - new Date(a.date));
-    
-            function formatDate(date) {
-                return new Intl.DateTimeFormat('pt-BR').format(new Date(date));
-            }
 
-            // Populate the table with the sorted data
+            filteredData.sort((a, b) => new Date(b.date) - new Date(a.date));
+
             filteredData.forEach(entry => {
                 const row = document.createElement('tr');
                 row.innerHTML = `
-                    <td>${formatDate(entry.date)}</td>
+                    <td>${entry.date}</td>
                     <td>${entry.entrada || ''}</td>
                     <td>${entry.intervalo || ''}</td>
                     <td>${entry.retorno || ''}</td>
