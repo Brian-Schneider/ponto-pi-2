f6c476dec50064eeadcd1b4d22a303987ab967c2
Brian Schneider de Lima <bschneiderlima@gmail.com>
2024-10-29 15:04:19 -0700

    Forty nineth commit

--- scripts/relatorio.js
+++ scripts/relatorio.js
@@ -53,8 +53,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const reportTableBody = document.querySelector('#history-table tbody');
     const baseUrl = 'http://127.0.0.1:5000';
 
+    const debounce = (func, delay) => {
+        let debounceTimer;
+        return function() {
+            const context = this;
+            const args = arguments;
+            clearTimeout(debounceTimer);
+            debounceTimer = setTimeout(() => func.apply(context, args), delay);
+        };
+    };
+
+
     async function fetchReport(employee, period) {
         try {
+            reportTableBody.innerHTML = '<tr><td colspan="6">Loading...</td></tr>'; // Show loading indicator
+
             const response = await fetch(`${baseUrl}/relatorio?employee=${employee}&period=${period}`);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -77,13 +90,16 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         } catch (error) {
             console.error('Error:', error);
+            reportTableBody.innerHTML = '<tr><td colspan="6">Error loading data</td></tr>'; // Show error message
+
         }
     }
 
-    filterButton.addEventListener('click', () => {
+    filterButton.addEventListener('click', debounce(() => {
         const periodStart = periodStartSelect.value;
         const periodEnd = periodEndSelect.value;
-        const employee = employeeInput.value;
+        const employee = employeeInput.value.trim();
+
         if (!periodStart || !periodEnd) {
             alert('Por favor, selecionar as datas.');
             return;
@@ -91,7 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const period = periodStart + ',' + periodEnd;
         fetchReport(employee, period);
-    });
+    }, 300));
 
 
     function updateTime() {
